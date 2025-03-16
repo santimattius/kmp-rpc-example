7b5d0ba68da8f718500a3d1a9d7575c36a6be706
Santiago Mattiauda <santimattius@gmail.com>
2025-03-15 21:50:35 -0300

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,20 @@
+import androidx.compose.runtime.Composable
+import coil3.ImageLoader
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.network.ktor3.KtorNetworkFetcherFactory
+import com.santimattius.kmp.compose.MainApplication
+import com.santimattius.kmp.compose.core.ui.themes.AppTheme
+
+@Composable
+fun App() {
+    setSingletonImageLoaderFactory { context ->
+        ImageLoader.Builder(context)
+            .components {
+                add(KtorNetworkFetcherFactory())
+            }
+            .build()
+    }
+    AppTheme {
+        MainApplication()
+    }
+}
